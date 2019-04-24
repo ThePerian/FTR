8438e2f0f793d69d747e7e749d32cb689d860a98
John Doe <h.perian@gmail.com>
2019-04-25 01:07:04 +0300

    Beginning of POI spawning. Add AreaManager.

---
+++ Assets/Scripts/AreaManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaManager : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+    public Collider2D spawnArea;
+    public int numeberOfPoints;
+    
+    void Start()
+    {
+        float minSpawnDistance = objectToSpawn.GetComponent<CircleCollider2D>().radius * 2;
+        Debug.Log(minSpawnDistance);
+        List<Vector2> existingPoints = new List<Vector2>();
+
+        for (int i = 0; i < numeberOfPoints; i++)
+        {
+            Vector2 randomPosition;
+            bool isTooClose;
+
+            do
+            {
+                randomPosition = new Vector2(
+                    Random.Range(spawnArea.bounds.min.x, spawnArea.bounds.max.x),
+                    Random.Range(spawnArea.bounds.min.y, spawnArea.bounds.max.y));
+
+                isTooClose = false;
+                foreach (var point in existingPoints)
+                {
+                    if (Vector2.Distance(randomPosition, point) < minSpawnDistance)
+                        isTooClose = true;
+                }
+            } while (!spawnArea.OverlapPoint(randomPosition) || isTooClose);
+
+            existingPoints.Add(randomPosition);
+            Instantiate(objectToSpawn, (Vector3)randomPosition, Quaternion.identity);
+        }
+    }
+    
+    void Update()
+    {
+        
+    }
+}
