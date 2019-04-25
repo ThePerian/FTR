008d59de3af28b05aa394889208109be5a91fdad
John Doe <h.perian@gmail.com>
2019-04-26 01:15:05 +0300

    Separate PointOfInterest entity from GlobalMapController.

---
+++ Assets/Scripts/GlobalMapController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalMapController : MonoBehaviour
+{
+    public GameObject pointOfInterest;
+    public int numberOfPoints;
+    public Collider2D spawnArea;
+    public Canvas parentCanvas;
+    public List<Collider2D> localAreas;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        float minDistance = pointOfInterest.GetComponent<CircleCollider2D>().radius * 2;
+        SpawnPOIs(pointOfInterest, numberOfPoints, spawnArea, minDistance, parentCanvas);
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    public void SpawnPOIs(GameObject objectToSpawn, int numberOfPoints,
+        Collider2D spawnArea, float minDistance, Canvas parentCanvas)
+    {
+        List<PointOfInterest> existingPoints = new List<PointOfInterest>();
+
+        for (int i = 0; i < numberOfPoints; i++)
+        {
+            Vector2 randomPosition;
+            bool isTooClose;
+
+            do
+            {
+                randomPosition = new Vector2(
+                    Random.Range(spawnArea.bounds.min.x, spawnArea.bounds.max.x),
+                    Random.Range(spawnArea.bounds.min.y, spawnArea.bounds.max.y));
+
+                isTooClose = false;
+                foreach (var point in existingPoints)
+                {
+                    if (Vector2.Distance(randomPosition, point.position) < minDistance)
+                        isTooClose = true;
+                }
+            } while (!spawnArea.OverlapPoint(randomPosition) || isTooClose);
+
+            GameObject newPoint = 
+                Instantiate(objectToSpawn, randomPosition, Quaternion.identity, parentCanvas.transform);
+
+            foreach(var area in localAreas)
+            {
+                if (newPoint.GetComponent<Collider2D>().Distance(area).isOverlapped)
+                {
+                    newPoint.tag = area.tag;
+                    break;
+                }
+            }
+
+            existingPoints.Add(new PointOfInterest()
+            {
+                position = randomPosition,
+                tag = newPoint.tag
+            });
+        }
+    }
+}
---
+++ Assets/Scripts/PointController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointController : MonoBehaviour
+{
+    public Sprite unexplored;
+    public Sprite explored;
+    public Sprite highlighted;
+    public Sprite clicked;
+    public bool isExplored = false;
+
+    Button button;
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(ButtonClicked);
+        //button.GetComponent<Image>().sprite = unexplored;
+    }
+    
+    void Update()
+    {
+        
+    }
+    /*
+    private void OnMouseEnter()
+    {
+        button.GetComponent<Image>().sprite = highlighted;
+    }
+
+    private void OnMouseExit()
+    {
+        button.GetComponent<Image>().sprite = isExplored ? explored : unexplored;
+    }
+
+    private void OnMouseDown()
+    {
+        button.GetComponent<Image>().sprite = clicked;
+    }
+    */
+    void ButtonClicked()
+    {
+        isExplored = true;
+    }
+    
+}
