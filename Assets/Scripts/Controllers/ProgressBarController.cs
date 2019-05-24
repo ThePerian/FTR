bcb8c89b738177da1df6e6260102cb82d8da8b97
John Doe <h.perian@gmail.com>
2019-05-24 21:54:47 +0300

    Add controller for progress bars.

---
+++ Assets/Scripts/Controllers/ProgressBarController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBarController : MonoBehaviour
+{
+    public Image mask;
+    public float minValue;
+    public float maxValue;
+    public float currentValue;
+
+    float originalBarSize;
+
+    private void Start()
+    {
+        originalBarSize = mask.rectTransform.rect.width;
+        //reset widths of background bars in case of incorrect sizes due to resolution change
+        foreach (var image in mask.gameObject.GetComponentsInChildren<Image>())
+            image.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, originalBarSize);
+    }
+
+    private void Update()
+    {
+        mask.rectTransform.SetSizeWithCurrentAnchors(
+            RectTransform.Axis.Horizontal,
+            originalBarSize * currentValue / maxValue);
+    }
+}
