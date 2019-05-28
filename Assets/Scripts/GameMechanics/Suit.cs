4cbfe6da8d5a9bf591c9b3a2cf1dc94edee0912e
John Doe <h.perian@gmail.com>
2019-05-28 22:27:38 +0300

    Add basic class for suits.

---
+++ Assets/Scripts/GameMechanics/Suit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Suit : InventoryItem
+{
+    public int maxHealth;
+    public int currentHealth;
+    public int damageThreshold;
+    public Dictionary<DamageType, int> damageResistance;
+    public bool hasFlashlight;
+    public bool hasNightVision;
+    public bool isHealing; //TODO: think of something better to store buffs of mutated suits
+    public bool isRigid;
+    public bool isExoskeleton;
+    public bool hasBreathingSystem;
+    public bool hasDisguise;
+    public bool hasHelmet;
+    public InventoryItem helmet;
+    public InventoryItem flashlight;
+    public InventoryItem nightVision;
+}
