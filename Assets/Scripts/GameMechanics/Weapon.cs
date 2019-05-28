7f482f975a70b75532b2de56325dff51d085c08b
John Doe <h.perian@gmail.com>
2019-05-28 22:18:33 +0300

    Add basic class for weapons.

---
+++ Assets/Scripts/GameMechanics/Weapon.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : InventoryItem
+{
+    public int damage;
+    public DamageType damageType;
+    public int maxHealth;
+    public int currentHealth;
+    public RangeInt jammingRange;
+    public float firingDistance;
+    public int maxAmmo;
+    public int currentAmmo;
+    public string caliber;
+    public int accuracy;
+}
